70db2e0a928937d57a3ad1b7f0b7e7c96aabad67
sullivanpaige1 <sul23003@byui.edu>
2026-01-15 21:41:38 -0600

    updates

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> words = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (Press 0 to exit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                words.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in words)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+                float average = ((float)sum) / words.Count;
+        Console.WriteLine($"The average is: {average}");
+        
+        int max = words[0];
+
+        foreach (int number in words)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
