d65efb06b682a9e33da8cab8f6c4e03f06eb6c6d
sullivanpaige1 <sul23003@byui.edu>
2026-02-14 18:07:35 -0600

    week06

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
-class Program
+Reference reference = new Reference("Proverbs", 3, 5, 6);
+Scripture scripture = new Scripture(reference,
+    "Trust in the Lord with all thine heart and lean not unto thine own understanding...in all thy ways acknowledge him, and he shall direct thy paths");
+
+while (!scripture.IsCompletelyHidden())
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop03 World!");
-    }
+    Console.Clear();
+    Console.WriteLine(scripture.GetDisplayText());
+    Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit:");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "quit")
+        break;
+
+    scripture.HideRandomWords(3);
 }
+
---
+++ prove/Develop03/Scripture/Scripture.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] parts = text.Split(" ");
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        Random random = new Random();
+
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string referenceText = _reference.GetDisplayText();
+        string scriptureText = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{referenceText}\n{scriptureText}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
